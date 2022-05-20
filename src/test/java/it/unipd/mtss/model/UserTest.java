ec65c841e447521e56d9edb96267aeed1ae4b5ca
Marco Volpato <marco@mvolpato.xyz>
2022-05-20 18:10:17 +0200

    aggiunti test utente

--- src/test/java/it/unipd/mtss/model/UserTest.java
+++ src/test/java/it/unipd/mtss/model/UserTest.java
@@ -5,6 +5,44 @@
 
 package it.unipd.mtss.model;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import static org.junit.Assert.assertEquals;
+
 public class UserTest {
-    
+    private User testUser;
+
+    @Before
+    public void setUp() {
+        testUser = new User(
+                1,
+                "Someone",
+                new GregorianCalendar(2022, Calendar.MAY, 20).getTime()
+        );
+    }
+
+    @Test
+    public void testGetId() {
+        assertEquals(1, testUser.getId());
+    }
+
+    @Test
+    public void testGetName() {
+        assertEquals("Someone", testUser.getName());
+    }
+
+    @Test
+    public void testGetBirth() {
+        Date expected = new GregorianCalendar(
+                2022,
+                Calendar.MAY,
+                20
+        ).getTime();
+        assertEquals(expected, testUser.getBirth());
+    }
 }
