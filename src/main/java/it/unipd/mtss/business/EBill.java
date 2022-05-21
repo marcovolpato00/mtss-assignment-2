51702252520bb79d04c53da35e306a817e98ef9a
Davide-Milan <davidemilan27@gmail.com>
2022-05-21 12:42:46 +0200

    close #2; sconto-processori

--- src/main/java/it/unipd/mtss/business/EBill.java
+++ src/main/java/it/unipd/mtss/business/EBill.java
@@ -8,13 +8,48 @@ package it.unipd.mtss.business;
 import java.util.List;
 import it.unipd.mtss.business.exception.BillException;
 import it.unipd.mtss.model.EItem;
+import it.unipd.mtss.model.EItemType;
 import it.unipd.mtss.model.User;
 
 public class EBill implements Bill {
+
+    /*
+    ======================
+    ====== ISSUE #2 ======
+    ======================
+    */
+    public double getCPUDiscount(List<EItem> itemsOrdered){
+        double discountCPU = 0;
+        long cpuCount = itemsOrdered
+                .stream()
+                .filter(item -> item.getItemType() == EItemType.PROCESSORE)
+                .count();
+
+        if(cpuCount > 5){
+            EItem cheapest = itemsOrdered
+                    .stream()
+                    .filter(item -> item.getItemType() == EItemType.PROCESSORE)
+                    .reduce((a, b) -> a.getPrice() < b.getPrice() ? a : b)
+                    .get();
+            discountCPU = (cheapest.getPrice() / 2);
+            System.out.println(discountCPU);
+        }
+        return discountCPU;
+    }
+
+
+    /*
+        ======================
+        ==== COSTO TOTALE ====
+        ======================
+    */
+
     @Override
     public double getOrderPrice(List<EItem> itemsOrdered, User user)
             throws BillException {
         double total = itemsOrdered.stream().mapToDouble(EItem::getPrice).sum();
-        return total;
+        double discountCPU = getCPUDiscount(itemsOrdered);
+        return total - discountCPU;
     }
+
 }
--- src/test/java/it/unipd/mtss/business/EBillTest.java
+++ src/test/java/it/unipd/mtss/business/EBillTest.java
@@ -38,6 +38,114 @@ public class EBillTest {
                 user
         );
 
-        assertEquals(199.90 + 98.50 + 20.10 + 449.50, total, 0.0);
+        assertEquals(199.90 + 98.50 + 20.10 + 449.50, total, 0.1);
+    }
+
+    /*
+        ======================
+        ====== ISSUE #2 ======
+        ======================
+    */
+    @Test
+    public void checkCPUDiscount_zeroCPU() throws BillException {
+
+        EBill bill = new EBill();
+        List<EItem> items = Arrays.asList(
+                new EItem(EItemType.SCHEDAMADRE, "Mobo 1", 98.50),
+                new EItem(EItemType.MOUSE, "Topolino", 20.10),
+                new EItem(EItemType.TASTIERA, "Razer SuperKeys 7000", 449.50)
+        );
+        User user = new User(
+                1,
+                "Jesus",
+                new GregorianCalendar(1000, Calendar.DECEMBER, 25).getTime()
+        );
+        double total = bill.getOrderPrice(
+                items,
+                user
+        );
+
+        assertEquals(98.50 + 20.10 + 449.50, total, 0.1);
+    }
+
+    @Test
+    public void checkCPUDiscount_lessThanFiveCPU() throws BillException {
+
+        EBill bill = new EBill();
+        List<EItem> items = Arrays.asList(
+                new EItem(EItemType.PROCESSORE, "Intel i5 1111k", 111.90),
+                new EItem(EItemType.SCHEDAMADRE, "Mobo 1", 98.50),
+                new EItem(EItemType.PROCESSORE, "Intel i5 2222k", 222.90),
+                new EItem(EItemType.PROCESSORE, "Intel i5 3333k", 333.90),                
+                new EItem(EItemType.MOUSE, "Topolino", 20.10),
+                new EItem(EItemType.PROCESSORE, "Intel i5 4444k", 444.90),
+                new EItem(EItemType.TASTIERA, "Razer SuperKeys 7000", 449.50)
+        );
+        User user = new User(
+                1,
+                "Jesus",
+                new GregorianCalendar(1000, Calendar.DECEMBER, 25).getTime()
+        );
+        double total = bill.getOrderPrice(
+                items,
+                user
+        );
+
+        assertEquals(111.90 + 222.90 + 333.90 + 444.90 + 98.50 + 20.10 + 449.50, total, 0.1);
+    }
+
+    @Test
+    public void checkCPUDiscount_FiveCPU() throws BillException {
+
+        EBill bill = new EBill();
+        List<EItem> items = Arrays.asList(
+                new EItem(EItemType.PROCESSORE, "Intel i5 1111k", 111.90),
+                new EItem(EItemType.SCHEDAMADRE, "Mobo 1", 98.50),
+                new EItem(EItemType.PROCESSORE, "Intel i5 2222k", 222.90),
+                new EItem(EItemType.PROCESSORE, "Intel i5 3333k", 333.90),                
+                new EItem(EItemType.MOUSE, "Topolino", 20.10),
+                new EItem(EItemType.PROCESSORE, "Intel i5 4444k", 444.90),
+                new EItem(EItemType.TASTIERA, "Razer SuperKeys 7000", 449.50),
+                new EItem(EItemType.PROCESSORE, "Intel i5 5555k", 555.90)
+        );
+        User user = new User(
+                1,
+                "Jesus",
+                new GregorianCalendar(1000, Calendar.DECEMBER, 25).getTime()
+        );
+        double total = bill.getOrderPrice(
+                items,
+                user
+        );        
+
+        assertEquals(111.90 + 222.90 + 333.90 + 444.90 + 555.90 + 98.50 + 20.10 + 449.50, total, 0.1);
+    }
+
+    @Test
+    public void checkCPUDiscount_moreThanFiveCPU() throws BillException {
+
+        EBill bill = new EBill();
+        List<EItem> items = Arrays.asList(
+                new EItem(EItemType.PROCESSORE, "Intel i5 1111k", 111.90),
+                new EItem(EItemType.SCHEDAMADRE, "Mobo 1", 98.50),
+                new EItem(EItemType.PROCESSORE, "Intel i5 2222k", 222.90),
+                new EItem(EItemType.PROCESSORE, "Intel i5 3333k", 333.90),                
+                new EItem(EItemType.MOUSE, "Topolino", 20.10),
+                new EItem(EItemType.PROCESSORE, "Intel i5 4444k", 444.90),
+                new EItem(EItemType.TASTIERA, "Razer SuperKeys 7000", 449.50),
+                new EItem(EItemType.PROCESSORE, "Intel i5 5555k", 555.90),
+                new EItem(EItemType.PROCESSORE, "Intel i5 6666k", 666.90)
+        );
+        User user = new User(
+                1,
+                "Jesus",
+                new GregorianCalendar(1000, Calendar.DECEMBER, 25).getTime()
+        );
+        double total = bill.getOrderPrice(
+                items,
+                user
+        );
+        double ass = (111.90 / 2) + 222.90 + 333.90 + 444.90 + 555.90 + 666.90 + 98.50 + 20.10 + 449.50;
+        assertEquals(ass, total, 0.0);
     }
 }
